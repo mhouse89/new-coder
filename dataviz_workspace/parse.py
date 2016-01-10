e5d1b797d6da64bd16666daff99252039250abb1
mhouse89 <matthew.house89@gmail.com>
2016-01-10 14:42:13 -0500

    Added graph.py

--- dataviz_workspace/parse.py
+++ dataviz_workspace/parse.py
@@ -1,6 +1,6 @@
 import csv
 
-MY_FILE = "../data/sample_sfpd_incident_all.csv"
+MY_FILE = "../dataviz/data/sample_sfpd_incident_all.csv"
 
 def parse(raw_file, delimiter):
     """Parses a raw CSV file to a JSON-line object."""
